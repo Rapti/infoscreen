8f28d584a75dbbb5888a1186ec318039019546ae
Leon Hoos <leon@die-hoosen.de>
2019-08-10 14:05:30 +0200

    Use different PublicTransit API and display platform

--- src/ModulePublicTransitStop.cpp
+++ src/ModulePublicTransitStop.cpp
@@ -12,6 +12,18 @@
 #include <ctime>
 #include <algorithm>
 
+
+ModulePublicTransitStop::ModulePublicTransitStop(const int stopID) {
+	this->stopID = stopID;
+	mutex = new std::mutex;
+	thread = new std::thread(&ModulePublicTransitStop::refreshLoop, std::ref(*this));
+}
+
+ModulePublicTransitStop::ModulePublicTransitStop(const int stopID, std::vector<std::string> types): ModulePublicTransitStop(stopID) {
+	this->types = types;
+}
+
+
 ModulePublicTransitStop::ModulePublicTransitStop(const std::string &city, const std::string &stop): Module() {
     this->city = city;
     this->stop = stop;
@@ -75,6 +87,11 @@ void ModulePublicTransitStop::draw() {
 		text.setFillColor(Screen::singleton->getTheme()->getTextSecondary());
 		text.setPosition(timelinewidth + text.getGlobalBounds().width, text.getPosition().y);
 		text.setString(train->getDestination());
+		t->draw(text);
+
+		text.setFillColor(Screen::singleton->getTheme()->getTextPrimary());
+		text.setString("Gleis " + train->getPlatform());
+		text.setPosition(getDisplayWidth() - text.getLocalBounds().width - 2 * padding, text.getPosition().y);
 		t->draw(text);
 
 		text.setFillColor(Screen::singleton->getTheme()->getTextPrimary());
@@ -189,139 +206,157 @@ static size_t writefunction(char *data, size_t size, size_t nmemb,
 void ModulePublicTransitStop::refreshLoop() {
 	active = true;
 	while(active) {
-		CURL* curl = curl_easy_init();
-
-		const char* esc_city = curl_easy_escape(curl, city.c_str(), city.length());
-		const char* esc_stop = curl_easy_escape(curl, stop.c_str(), stop.length());
-		std::stringstream ss;
-		ss << "https://vrrf.finalrewind.org/" << esc_city << "/" << esc_stop << ".json";
-//		const char* url = ss.str().c_str();
-		char* url = new char[ss.str().length() +1];
-		std::strcpy(url, ss.str().c_str());
-//		std::cout << url<< std::endl;
-//		ss.str("");
-		std::string result;
-
-		curl_easy_setopt(curl, CURLOPT_URL, url);
-
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
-//		std::cout << "pointer: " << writefunction << std::endl;
-
-		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunction);
-
-		CURLcode res = curl_easy_perform(curl);
-//		std::cout << "curlcode: " << res << std::endl;
-
-
-		if(res != CURLE_OK) {
-			fprintf(stderr, "curl_easy_perform() failed: %s\n",
-					curl_easy_strerror(res));
-			curl_easy_cleanup(curl);
-			break;
-		}
-
-		curl_easy_cleanup(curl);
-
-//		std::cout << "result: " << result << std::endl;
 		rapidjson::Document data;
-		data.Parse(result.c_str());
-		mutex->lock();
+		try {
+			CURL* curl = curl_easy_init();
 
-		for(auto i = trains.begin(); i != trains.end();) {
-			if((*i)->getIndex() < 0 && (*i)->getPreviousIndex() < 0) { // Remove trains that are off-screen.
-				delete *i;
-				i = trains.erase(i);
-			} else {
-				(*i)->setInvalid(true); // will be set to false again if the train still appears in the next json
-				++i;
+			std::stringstream ss;
+			if(stopID > 0)
+				ss << "http://openservice-test.vrr.de/static02/XML_DM_REQUEST?outputFormat=JSON&mode=direct&sessionID=0&requestID=0&language=DE&locationServerActive=1&useRealtime=1&limit=100&type_dm=stopID&name_dm=" << stopID;
+	//		const char* url = ss.str().c_str();
+			char* url = new char[ss.str().length() +1];
+			std::strcpy(url, ss.str().c_str());
+			std::cout << url<< std::endl;
+	//		ss.str("");
+			std::string result;
+
+			curl_easy_setopt(curl, CURLOPT_URL, url);
+
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
+	//		std::cout << "pointer: " << writefunction << std::endl;
+
+			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunction);
+
+			CURLcode res = curl_easy_perform(curl);
+	//		std::cout << "curlcode: " << res << std::endl;
+
+
+			if(res != CURLE_OK) {
+				fprintf(stderr, "curl_easy_perform() failed: %s\n",
+						curl_easy_strerror(res));
+				curl_easy_cleanup(curl);
+				break;
 			}
+			curl_easy_cleanup(curl);
+			data.Parse(result.c_str());
+		} catch (std::exception e) {
+
 		}
 
-		if(false && !trains.empty()) {
-			trains.front()->setInvalid(true);
-			for(auto train: trains)
-				train->markAsUpdated();
-		} else {
-			for (rapidjson::Value::ValueIterator i = data["raw"].Begin(); i != data["raw"].End(); ++i) {
-				Train* t = nullptr;
-				rapidjson::GenericValue<rapidjson::UTF8<>, rapidjson::MemoryPoolAllocator<>> &jsonTrain = *i;
+		if(!data.HasParseError()) {
+//		std::cout << "result: " << result << std::endl;
+			mutex->lock();
+
+			for (auto i = trains.begin(); i != trains.end();) {
+				if ((*i)->getIndex() < 0 && (*i)->getPreviousIndex() < 0) { // Remove trains that are off-screen.
+					delete *i;
+					i = trains.erase(i);
+				} else {
+					(*i)->setInvalid(true); // will be set to false again if the train still appears in the next json
+					++i;
+				}
+			}
+
+			if (false && !trains.empty()) {
+				trains.front()->setInvalid(true);
+				for (auto train: trains)
+					train->markAsUpdated();
+			} else {
+				for (rapidjson::Value::ValueIterator i = data["departureList"].Begin();
+					 i != data["departureList"].End(); ++i) {
+					Train* t = nullptr;
+					rapidjson::GenericValue<rapidjson::UTF8<>, rapidjson::MemoryPoolAllocator<>> &jsonTrain = *i;
+					rapidjson::GenericObject<false, rapidjson::GenericValue<rapidjson::UTF8<char>, rapidjson::MemoryPoolAllocator<rapidjson::CrtAllocator>>::ValueType> jsonLine = jsonTrain["servingLine"].GetObject();
+					rapidjson::GenericObject<false, rapidjson::GenericValue<rapidjson::UTF8<char>, rapidjson::MemoryPoolAllocator<rapidjson::CrtAllocator>>::ValueType> jsonTime = jsonTrain["dateTime"].GetObject();
 
 
-				if(!types.empty()) {
-					bool accept = false;
-					for (auto type = types.begin(); type != types.end(); ++type) {
-						if (jsonTrain["lineref"]["type"].GetString() == *type) {
-							accept = true;
+					if (!types.empty()) {
+						bool accept = false;
+						for (auto type = types.begin(); type != types.end(); ++type) {
+							if (jsonLine["name"].GetString() == *type) {
+								accept = true;
+								break;
+							}
+						}
+						if (!accept)
+							continue;
+					}
+
+
+					struct std::tm tm;
+					tm.tm_sec = 0;
+					tm.tm_min = 0;
+					tm.tm_hour = 0;
+					tm.tm_mday = 0;
+					tm.tm_wday = 0;
+					tm.tm_yday = 0;
+					tm.tm_mon = 0;
+					tm.tm_year = 0;
+//				tm.tm_hour = jsonTime["hour"].GetInt();
+//				tm.tm_mday = jsonTime["day"].GetInt();
+////				tm.tm_wday = jsonTime["minute"].GetInt();
+////				tm.tm_yday = jsonTime["minute"].GetInt();
+//				tm.tm_mon = jsonTime["month"].GetInt();
+//				tm.tm_year = jsonTime["year"].GetInt();
+					tm.tm_isdst = 0;
+
+					std::stringstream ss;
+					ss << std::setw(2) << std::setfill('0') << jsonTime["day"].GetString() << ".";
+					ss << std::setw(2) << std::setfill('0') << jsonTime["month"].GetString() << ".";
+					ss << std::setw(4) << std::setfill('0') << jsonTime["year"].GetString() << " ";
+					ss << std::setw(2) << std::setfill('0') << jsonTime["hour"].GetString() << ":";
+					ss << std::setw(2) << std::setfill('0') << jsonTime["minute"].GetString();
+					mktime(&tm);
+					std::cout << ss.str() << std::endl;
+					ss >> std::get_time(&tm, "%d.%m.%Y %H:%M");
+					time_t time = mktime(&tm);
+					struct tm* localtm = gmtime(&time);
+					time = mktime(localtm);
+
+					std::string id =
+							std::string(jsonLine["stateless"].GetString()) + " " + jsonTime["hour"].GetString() + ":" +
+							jsonTime["minute"].GetString();
+//				std::cout << id << std::endl;
+//				continue;
+					for (auto candidate: trains) {
+						if (candidate->getID().compare(id) == 0) {
+							t = candidate;
 							break;
 						}
 					}
-					if(!accept)
-						continue;
-				}
-
-
-				std::string id = std::string(jsonTrain["lineref"]["identifier"].GetString()) + " " +
-								 jsonTrain["sched_time"].GetString();
-				for (auto candidate: trains) {
-//				std::cout << "Comparing " << id << " to " << candidate->getID() << std::endl;
-					if (candidate->getID().compare(id) == 0) {
-						t = candidate;
-						break;
+					if (t == nullptr) {
+						t = new Train();
+						t->setID(id);
+						trains.push_back(t);
 					}
-				}
-				if (t == nullptr) {
-					t = new Train();
-					t->setID(id);
-					trains.push_back(t);
-				}
-				t->setInvalid(false);
-				t->setName(jsonTrain["line"].GetString());
-				std::string str(jsonTrain["destination"].GetString());
+					t->setInvalid(false);
+					if (jsonTrain.HasMember("platform")) {
+						t->setPlatform(jsonTrain["platform"].GetString());
+					} else {
+						t->setPlatform("");
+					}
+					t->setName(jsonLine["symbol"].GetString());
+					std::string str(jsonLine["direction"].GetString());
 
 
-				std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-				std::wstring wide = converter.from_bytes(str);
+					std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+					std::wstring wide = converter.from_bytes(str);
 
-				t->setDestination(wide);
+					t->setDestination(wide);
 
-				struct std::tm tm;
-				tm.tm_sec = 0;
-				tm.tm_min = 0;
-				tm.tm_hour = 0;
-				tm.tm_mday = 0;
-				tm.tm_wday = 0;
-				tm.tm_yday = 0;
-				tm.tm_mon = 0;
-				tm.tm_year = 0;
-				tm.tm_isdst = 0;
-				std::stringstream ss;
-				ss << jsonTrain["sched_date"].GetString();
-				ss << " ";
-				ss << jsonTrain["sched_time"].GetString();
-				mktime(&tm);
-				ss >> std::get_time(&tm, "%d.%m.%Y %H:%M");
-				tm.tm_sec = 0;
-				time_t time = mktime(&tm);
-				struct tm* localtm = gmtime(&time);
-				time = mktime(localtm);
 //			std::cout << ss.str() << std::endl;
 //			std::cout << asctime(localtime(&time)) << std::endl;
-				t->setDeparture(time);
-
-				if (!jsonTrain["delay"].IsNull()) {
-					if (jsonTrain["delay"].IsString())
-						t->setDelay(atoi(jsonTrain["delay"].GetString()));
-					else t->setDelay(jsonTrain["delay"].GetInt());
+					t->setDeparture(time);
+					int delay = 0;
+					if (jsonLine.HasMember("delay") && !jsonLine["delay"].IsNull()) {
+						if (jsonLine["delay"].IsString())
+							delay = atoi(jsonLine["delay"].GetString());
+						else delay = jsonLine["delay"].GetInt();
+					}
+					t->setCancelled(delay == -9999);
+					t->setDelay(delay == -9999? 0 : delay);
+					t->markAsUpdated();
 				}
-				if (!jsonTrain["is_cancelled"].IsNull()) {
-					if (jsonTrain["is_cancelled"].IsString())
-						t->setCancelled(atoi(jsonTrain["is_cancelled"].GetString()) == 1);
-					else if (jsonTrain["is_cancelled"].IsInt())
-						t->setCancelled(jsonTrain["is_cancelled"].GetInt() == 1);
-					else t->setCancelled(jsonTrain["is_cancelled"].GetBool());
-				}
-				t->markAsUpdated();
-			}
 
 
 //			for(int i = 0; i < 20; ++i) {  // Create some testing trains
@@ -354,34 +389,41 @@ void ModulePublicTransitStop::refreshLoop() {
 //			}
 
 
-		}
-		trains.sort( []( Train *a, Train *b ) {
-			if(a->isInvalid() && !b->isInvalid()) return true;
-			if(b->isInvalid() && !a->isInvalid()) return false;
-			if(a->getDeparture() + a->getDelay() * 60 == b->getDeparture() + b->getDelay() * 60)
-				return a->getDeparture() < b->getDeparture();
-			return a->getDeparture() + a->getDelay() * 60 < b->getDeparture() + b->getDelay() * 60;
-		});
-		int i = 0;
-		time_t now;
-		std::time(&now);
-		for(Train* t: trains)  {
-			t->markAsUpdated();
-			if(t->isInvalid() || ceil(difftime(t->getDeparture(), now) / 60) + t->getDelay() < -1) {
-				--i;
-//				std::cout << "Train " << t->getID() << " is invalid" << std::endl;
-				std::cout << ceil(difftime(t->getDeparture(), now) / 60) + t->getDelay() << std::endl;
 			}
-		}
-		for(Train* t: trains)  {
-			t->setIndex(i++);
-		}
-		mutex->unlock();
+			trains.sort([](Train* a, Train* b) {
+				if (a->isInvalid() && !b->isInvalid()) return true;
+				if (b->isInvalid() && !a->isInvalid()) return false;
+				if (a->getDeparture() + a->getDelay() * 60 == b->getDeparture() + b->getDelay() * 60)
+					return a->getDeparture() < b->getDeparture();
+				return a->getDeparture() + a->getDelay() * 60 < b->getDeparture() + b->getDelay() * 60;
+			});
+			int i = 0;
+			time_t now;
+			std::time(&now);
+			for (Train* t: trains) {
+				t->markAsUpdated();
+				if (t->isInvalid() || ceil(difftime(t->getDeparture(), now) / 60) + t->getDelay() < -1) {
+					--i;
+//				std::cout << "Train " << t->getID() << " is invalid" << std::endl;
+					time_t time = t->getDeparture();
+					struct tm tm;
+					tm = *localtime(&time);
+					std::cout << ceil(difftime(t->getDeparture(), now) / 60) + t->getDelay() << std::endl;
+					std::cout << tm.tm_mday << "." << tm.tm_mon << "." << tm.tm_year << " " << tm.tm_hour << ":"
+							  << tm.tm_min << " " << t->getDelay() << std::endl;
+				}
+			}
+			for (Train* t: trains) {
+				t->setIndex(i++);
+			}
 
-		int d = 5000 - clock.getElapsedTime().asMilliseconds();
+			mutex->unlock();
+
+		}
+
+		int d = 15000 - clock.getElapsedTime().asMilliseconds();
 		if (d > 0)
 			std::this_thread::sleep_for(std::chrono::milliseconds(d));
 		clock.restart();
 	}
 }
-
--- src/ModulePublicTransitStop.h
+++ src/ModulePublicTransitStop.h
@@ -16,17 +16,20 @@ class ModulePublicTransitStop: public Module {
 private:
     std::string city;
     std::string stop;
+    int stopID = 0;
 	std::list<Train*> trains;
 	std::vector<std::string> types;
 
 
 	void refreshLoop();
-	bool active;
+	bool active = false;
 	std::thread* thread;
 	std::mutex* mutex;
 	sf::Clock clock;
 
 public:
+    ModulePublicTransitStop(const int stopID);
+    ModulePublicTransitStop(const int stopID, std::vector<std::string> types);
     ModulePublicTransitStop(const std::string &city, const std::string &stop);
     ModulePublicTransitStop(const std::string &city, const std::string &stop, std::vector<std::string> types);
 
