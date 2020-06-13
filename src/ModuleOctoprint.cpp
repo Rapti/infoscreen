02f40af40a4728ad1d02695958bb6adb0975ebac
Leon Hoos <leon@die-hoosen.de>
2020-06-13 16:19:37 +0200

    Implement data retrieval

--- src/ModuleOctoprint.cpp
+++ src/ModuleOctoprint.cpp
@@ -2,4 +2,93 @@
 // Created by leon on 13.06.20.
 //
 
+#include <rapidjson/document.h>
+#include <curl/curl.h>
 #include "ModuleOctoprint.h"
+
+ModuleOctoprint::ModuleOctoprint(const string &apiUrl, const string &apiKey) : apiUrl(apiUrl), apiKey(apiKey) {
+	cout << "Instantiating Octoprint Module" << endl;
+	active = true;
+	refreshThread = new std::thread(&ModuleOctoprint::refreshLoop, std::ref(*this));}
+
+ModuleOctoprint::~ModuleOctoprint() {
+}
+
+void ModuleOctoprint::draw() {
+
+}
+
+static size_t writefunction(char *data, size_t size, size_t nmemb,
+							std::string *writerData) {
+	if(writerData == NULL)
+		return 0;
+	writerData->append(data, size*nmemb);
+
+	return size * nmemb;
+}
+
+void ModuleOctoprint::refreshLoop() {
+
+	char apiKeyHeader[apiKey.length() +12];
+	strcpy(apiKeyHeader, ("X-Api-Key: " + apiKey).c_str());
+
+	while(active) {
+		rapidjson::Document data;
+		try {
+			CURL* curl = curl_easy_init();
+			std::string result;
+
+			curl_easy_setopt(curl, CURLOPT_URL, apiUrl.c_str());
+
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
+
+			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunction);
+
+			struct curl_slist *chunk = NULL;
+			/* Add a custom header */
+			chunk = curl_slist_append(chunk, "X-Api-Key: ");
+			/* set our custom set of headers */
+			curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+
+			CURLcode res = curl_easy_perform(curl);
+
+
+			if(res != CURLE_OK) {
+				fprintf(stderr, "curl_easy_perform() failed: %s\n",
+						curl_easy_strerror(res));
+				curl_easy_cleanup(curl);
+				active = false;
+				break;
+			}
+			curl_easy_cleanup(curl);
+			data.Parse(result.c_str());
+		} catch (std::exception e) {
+
+		}
+
+		if(!data.HasParseError()) {
+			m.lock();
+
+			auto job = data["job"].GetObject();
+			auto progress = data["progress"].GetObject();
+
+			if(job["file"].IsObject()) {
+				auto f = job["file"].GetObject();
+				if(f["display"].IsString())
+					file = f["display"].GetString();
+				else file = "null";
+			} else file = "";
+
+			if(progress["completion"].IsDouble())
+				jobProgress = progress["completion"].GetDouble();
+			else jobProgress = -1;
+			if(progress["printTime"].IsInt())
+				printTime = progress["printTime"].GetInt();
+			else printTime = -1;
+			if(progress["printTimeLeft"].IsInt())
+				printTimeLeft = progress["printTimeLeft"].GetInt();
+			else printTimeLeft = -1;
+		}
+	}
+}
+
--- src/ModuleOctoprint.h
+++ src/ModuleOctoprint.h
@@ -5,11 +5,31 @@
 #ifndef INFOSCREEN_MODULEOCTOPRINT_H
 #define INFOSCREEN_MODULEOCTOPRINT_H
 
+using namespace std;
 
+#include <thread>
 #include "Module.h"
 
 class ModuleOctoprint: public Module {
-	
+private:
+	const string apiUrl;
+	const string apiKey;
+	string status;
+	string file;
+	double jobProgress;
+	int printTime;
+	int printTimeLeft;
+
+	mutex m;
+	bool active;
+	thread* refreshThread;
+
+	void refreshLoop();
+
+public:
+	ModuleOctoprint(const string &apiUrl, const string &apiKey);
+	virtual ~ModuleOctoprint();
+	virtual void draw();
 };
 
 
